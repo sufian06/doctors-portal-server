4a96b8815dedeb35cde9cca210d5a44097b76e47
sufian <sufian4se@gmail.com>
2022-11-16 18:16:26 +0600

    initial server, mongodb

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ilq1t0j.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+app.get("/", async (req, res) => {
+  res.send("doctors portal server is running");
+});
+
+app.listen(port, () => console.log(`Doctors portal is running on ${port}`));
